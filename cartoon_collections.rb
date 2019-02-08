0562e5a0b4f5cf2ae734b84a41b2b268df32e397
Kylla Harris <kylla.harris@gmail.com>
2019-02-08 17:18:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,8 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(cheese)
-  cheeses = []
-  cheese.find do |types| 
-    if cheese_types.include?(types)
-      return cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |types| 
+   cheese_types.include?(types)
+    
 end
